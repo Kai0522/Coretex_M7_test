d6b7d492dcb37a8582f8abd36ae1851401eb2601
Kai0522 <kklin0522@gmail.com>
2019-03-29 23:45:13 +0800

    3/29 complete memory mapping

--- Cortex_M7_test/GPIO.c
+++ Cortex_M7_test/GPIO.c
@@ -1,7 +1,6 @@
 #include <stdint.h>
 #include "reg.h"
 #include "GPIO.h"
-int a=1;
 
 void pinMode(unsigned int port,unsigned int pin,unsigned int mode){
 	if(mode==1){
--- Cortex_M7_test/main.c
+++ Cortex_M7_test/main.c
@@ -1,15 +1,17 @@
 #include "GPIO.h"
 #include "reg.h"
-int i;
+int j=522;
+int i=500;
+int bss_test;
 
 int main(){
     pinMode(GPIO_PORTI,1,OUTPUT);
     while(1){
      digitalWrite(GPIO_PORTI,1,HIGH);
-     for(i=0;i<1000000;i++)
+     for(i=0;i<100000;i++)
          ;
      digitalWrite(GPIO_PORTI,1,LOW);
-     for(i=0;i<1000000;i++)
+     for(i=0;i<100000;i++)
          ;
     }
 }
